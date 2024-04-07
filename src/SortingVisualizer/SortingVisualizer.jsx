19627b49cb81a9dc6db84366e51c8a79fadfe724
Navdeep <108007267+nxvtej@users.noreply.github.com>
2024-04-07 14:28:22 +0530

    generating array from 5 to 1000 set and returing

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -14,5 +14,28 @@ export default class SortingVisualizer extends React.Component {
     this.resetArray();
   }
 
+  resetArray(){
+    const array = [];
+    
+    for(let i =0; i <100; i++){
+        array.push(randomIntFromIntervel(5,1000));
+    }
+    this.setState({array});
+  }
+
+  render() {
+    const {array} = this.state;
+
+    return (
+        <>
+        {array.map((value, idx)=> (
+            <div className="array-bar" key={idx}>
+                {value}
+            </div>
+        ))}
+        </>
+    );
+  }
+
   
 }
