bc04e0a1aa9bb990350ae5f180afd134cc681e23
Navdeep <108007267+nxvtej@users.noreply.github.com>
2024-04-07 16:00:32 +0530

    testing random nubmer of arrays

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -23,6 +23,27 @@ export default class SortingVisualizer extends React.Component {
     }
     this.setState({array});
   }
+
+testSortingAlgorithms() {
+    for(let i=0;i<100;i++){
+        const array =[];
+        const cnttesting =randomIntFromIntervel(1,1000);
+        for(let i=0;i<cnttesting;i++){
+            array.push(randomIntFromIntervel(-1000,1000));
+        }
+        const javaScriptSortedArray = this.state.array
+        .slice()
+        .sort((a,b)=>a-b);
+        const sortedArray = sortingAlgorithms.mergeSort(this.state.array);
+    
+    
+        // also printing if theyare eqiual or not into console
+        console.log(arrayAreEqual(javaScriptSortedArray, sortedArray));
+    
+
+    }
+}
+
 mergeSort() {
     // generating copy of that aray to sort using inbuilt funciton
     const javaScriptSortedArray = this.state.array
@@ -68,6 +89,7 @@ cycleSort() {}
         <button onClick={() => this.quickSort()}>Quick Sort</button>
         <button onClick={() => this.heapSort()}>Heap Sort</button>
         <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+        <button onClick={() => this.testSortingAlgorithms()}>Test Sorting Algorithms</button>
 {/* do this by userlsef */}
         <button onClick={() => this.selectionSort()}>Selection Sort</button>
         <button onClick={() => this.insertionSort()}>Insertion Sort</button>
